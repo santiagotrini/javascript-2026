3f3b23fdd7a3dac75706d3ae63d6c2967646d26f
Santiago Trini <santiagotrini@gmail.com>
2026-08-28 08:29:19 -0300

    agrega ejercicio

---
+++ 04-isleap/main.js
@@ -0,0 +1,21 @@
+// hay que implementar una funcion isLeap(year) que devuelva true o false si
+// un año es bisiesto o no
+// reglas para ser año bisiesto: 1. sos divisible por 4
+//                               2. no sos divisible por 100
+//                               3. salvo que seas divisible por 400
+//                               4. si cumplis todas estas reglas sos bisiesto sino no
+
+// usar un loop para preguntar al usuario un año, decirle si es bisiesto y darle la 
+// opcion de volver a comenzar
+
+
+function isLeap(year) {
+  // tu codigo acá
+}
+
+function main() {
+  // la logica principal del programa
+  // completar...
+}
+
+main();
