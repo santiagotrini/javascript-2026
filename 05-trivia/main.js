92fcf0aa320cb357e3a1ef4d1dc30683fbbc40d7
Santiago Trini <santiagotrini@gmail.com>
2026-09-01 08:13:37 -0300

    agrega otro ejercicio

---
+++ 05-trivia/main.js
@@ -0,0 +1,16 @@
+// este ejercicio es para jugar una trivia
+// cada pregunta es un objeto asi
+let firstQuestion = {
+  questionText: 'Cual es la raiz cuadrada de 144?',
+  answerA: '9',
+  answerB: '100',
+  answerC: '12',
+  answerD: '25',
+  correctAnswer: '12'
+};
+// el programa debe mostrar al menos 4 preguntas, preguntar al usuario por una opcion
+// y acumular los puntos obtenidos en una variable
+
+// avance de la proxima temporada: usar arrays en este ejercicio para manejar un numero
+// arbitrario de preguntas (en la prueba igual no entra arrays)
+
--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Algunos ejercicios resueltos.
 ![por que es importante](romanos.gif)
 
 - [04-isleap](/04-isleap) practicando condiciones con cuarto segunda el 28/08.
+- [05-trivia](/05-trivia) practicando condiciones y objetos con cuarto segunda el 01/09.
 
 ### Fórmulas para el ejercicio de temperaturas
 
