fe3b7b2fa447cf324fb1f59d1247402ff180b575
Santiago Trini <santiagotrini@gmail.com>
2026-09-01 10:32:57 -0300

    agrega solucion

--- 06-timer/main.js
+++ 06-timer/main.js
@@ -3,3 +3,12 @@
 // pedirle al usuario que ingrese una cantidad de segundos, minutos, horas o lo que sea
 // arrancar una cuenta regresiva desde esa cantidad a cero
 // cuando llegue a cero avisarle al usuario con un alert
+let answer = prompt('Decime cuantos H/M/S queres esperar');
+let amount = answer.split(' ')[0];
+let unit = answer.split(' ')[1];
+if (unit == 'M') amount *= 60;
+else if (unit == 'H') amount *= 3600;
+// el timer
+setTimeout(() => {
+  alert('Tiempo!!!!');
+}, amount * 1000)
