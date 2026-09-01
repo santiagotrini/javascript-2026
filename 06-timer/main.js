6440d8e17e0509cc9116c3e0cf163455a94a5899
Santiago Trini <santiagotrini@gmail.com>
2026-09-01 09:30:33 -0300

    otro ejercicio

---
+++ 06-timer/main.js
@@ -0,0 +1,5 @@
+// este script implementa un timer o temporizador
+// como para saber cuando esta la torta que dejaste en el horno o algo de eso
+// pedirle al usuario que ingrese una cantidad de segundos, minutos, horas o lo que sea
+// arrancar una cuenta regresiva desde esa cantidad a cero
+// cuando llegue a cero avisarle al usuario con un alert
