66ca8e99f4f1870fe1e9ff0af26fec0e387a4265
Santiago Trini <santiagotrini@gmail.com>
2026-09-03 09:31:51 -0300

    agrega solucion

--- 07-primavera/main.js
+++ 07-primavera/main.js
@@ -9,3 +9,26 @@
 // bonus track: hacerlo en un documento HTML usando un h1 blanco sobre fondo rojo
 
 // poné tu solución acá
+
+const target = new Date('2026-09-21T00:00:00');
+const h1     = document.querySelector('h1');
+setInterval(() => {
+  const now    = new Date();
+  let diff       = target - now;
+  const weeks    = Math.floor(diff / (1000 * 60 * 60 * 24 * 7));
+  diff          %= 1000 * 60 * 60 * 24 * 7;
+  const days     = Math.floor(diff / (1000 * 60 * 60 * 24));
+  diff          %= 1000 * 60 * 60 * 24;
+  const hours    = Math.floor(diff / (1000 * 60 * 60));
+  diff          %= 1000 * 60 * 60;
+  const minutes  = Math.floor(diff / (1000 * 60));
+  diff          %= 1000 * 60;
+  const seconds  = Math.floor(diff / 1000);
+  h1.textContent = `FALTAN ${weeks} SEMANA${weeks == 1 ? '' : 'S'}, ` + 
+                   `${days} DIA${days == 1 ? '' : 'S'}, ` + 
+                   `${hours} HORA${hours == 1 ? '' : 'S'}, ` +  
+                   `${minutes} MINUTO${minutes == 1 ? '' : 'S'} Y ` + 
+                   `${seconds} SEGUNDO${seconds == 1 ? '' : 'S'} ` + 
+                   `PARA LA PRIMAVERA`;
+}, 1000);
+
