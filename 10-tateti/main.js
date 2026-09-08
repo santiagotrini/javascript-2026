8440d8ee8ab37a55c64fe90a9ae6556af3e3c042
Santiago Trini <santiagotrini@gmail.com>
2026-09-08 10:05:49 -0300

    agrega ejercicio imposible

---
+++ 10-tateti/main.js
@@ -0,0 +1,9 @@
+// tateti: tenes 9 botones y tenes 3 fichas para poner (cruces y circulos)
+// no podes poner mas de 3 fichas, no podes poner fichas si no esta vacio, se van turnando
+// los jugadores.
+
+// usar event bubbling para poner un handler para todo el tablero 
+
+function handleClick(e) {
+
+}
